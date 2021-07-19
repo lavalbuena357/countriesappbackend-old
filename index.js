06745d4100ff456aefeaab4333550205359afef4
albertvr357 <cuervo357@gmail.com>
2021-07-18 23:12:16 -0500

    fix

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
-conn.sync({ force: false }).then(() => {
-  server.listen(8000, () => {
-    console.log('listening at 8000'); // eslint-disable-line no-console
-  });
-});
+// conn.sync({ force: false }).then(() => {
+//   server.listen(8000, () => {
+//     console.log(`listening at 8000`); // eslint-disable-line no-console
+//   });
+// });
+
+server.listen(8000, () => {
+  console.log(`listening at 8000`);
+})
+
