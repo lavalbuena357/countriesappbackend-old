4d27f1dc36909c5836a053be2a5c53bc586157be
albertvr357 <cuervo357@gmail.com>
2021-07-19 00:02:43 -0500

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const port = process.env.PORT || 3000
 
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(port, () => {
     console.log(`listening at ${port}`); // eslint-disable-line no-console
   });
