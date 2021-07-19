e5ec91884cc55ca99722c4842d6f45d911ba474b
albertvr357 <cuervo357@gmail.com>
2021-07-19 01:38:52 -0500

    db

--- src/db.js
+++ src/db.js
@@ -2,17 +2,21 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const env = require('./env.js');
+const {
+  DB_USER, DB_PASSWORD, DB_HOST, PORT, DATABASE
+} = process.env;
 
-const sequelize = new Sequelize(`postgres://${env.username}:${env.password}@${env.host}/${env.database}`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  dialectOptions: {
-  ssl: {      /* <----- Add SSL option */
-    require: true,
-    rejectUnauthorized: false 
-    }
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DATABASE}`, {
+  dialect: 'postgresql',
+  pool: {
+    max: 20,
+    min: 0,
+    acquire: 60000,
+    idle: 10000
   },
+  logging: false, 
+  native: false, 
+  dialectOptions: {ssl: {require: true, rejectUnauthorized: false}},
 });
 const basename = path.basename(__filename);
 
