1257dc75601f7efb091027df2ed4023abaf5ad50
Albert Valbuena <44592599+lavalbuena357@users.noreply.github.com>
2021-07-19 11:48:21 -0500

    Add files via upload

---
+++ src/env.js
@@ -0,0 +1,9 @@
+const env = {
+  database: 'd5vobglcrmpjbk',
+  username: 'eowhvbhtddjzjf',
+  password: 'e12462b09103f2e318d70fff9840f4be10d7492d1d0ff078fbe5b41d51f97bde',
+  host: 'ec2-52-23-40-80.compute-1.amazonaws.com',
+  port: 5432
+};
+ 
+module.exports = env;
