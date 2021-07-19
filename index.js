6bc6a37dddb11b2dbe2ea455c95b21d575669b7f
albertvr357 <cuervo357@gmail.com>
2021-07-18 21:49:14 -0500

    db update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log('listening at 3001'); // eslint-disable-line no-console
+  server.listen(8000, () => {
+    console.log('listening at 8000'); // eslint-disable-line no-console
   });
 });
