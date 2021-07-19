48e228e7b3e267c1e7e192d62a08eda8da1e4097
albertvr357 <cuervo357@gmail.com>
2021-07-19 01:07:23 -0500

    db

--- src/db.js
+++ src/db.js
@@ -8,8 +8,8 @@ const sequelize = new Sequelize(`postgres://${env.username}:${env.password}@${en
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   dialectOptions: {
-    "ssl": true
-  }
+    ssl: true
+  },
 });
 const basename = path.basename(__filename);
 
