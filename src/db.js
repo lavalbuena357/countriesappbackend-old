dd0e397b9a5ab864342b55366b3a4c4fa52049a4
albertvr357 <cuervo357@gmail.com>
2021-07-19 01:05:53 -0500

    db

--- src/db.js
+++ src/db.js
@@ -7,6 +7,9 @@ const env = require('./env.js');
 const sequelize = new Sequelize(`postgres://${env.username}:${env.password}@${env.host}/${env.database}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialectOptions: {
+    "ssl": true
+  }
 });
 const basename = path.basename(__filename);
 
